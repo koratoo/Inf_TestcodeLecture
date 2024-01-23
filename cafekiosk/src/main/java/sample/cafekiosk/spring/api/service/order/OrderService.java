accba3fce95200f58d70497ee7627ddf9ed745fb
koratoo <ssyk2@naver.com>
2024-01-23 10:40:04 +0900

    중복상품 처리

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,12 +22,17 @@ public class OrderService {
     private final OrderRepository orderRepository;
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
 
-        List<String> productNumbers = request.getProductNumbers();
         //Product
+        List<String> productNumbers = request.getProductNumbers();
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber,p->p));
 
+        List<Product> duplicateProducts = productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
         //Order
-        Order order = Order.create(products, registeredDateTime);
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
         return OrderResponse.of(savedOrder);
     }
